b7fddeba4df3268dce7a2b023a48b4d20b161614
pranjal007 <bahuguna.pranjal@gmail.com>
2017-06-12 19:26:12 +0530

    server-setup

---
+++ config/database.js
@@ -0,0 +1,11 @@
+
+
+const crypto = require('crypto').randomBytes(256).toString('hex');
+
+module.exports={
+
+    uri:'mongodb://localhost:27017/'  + this.db,
+    secret:crypto,
+    db:'mean-angular-4'
+
+}
